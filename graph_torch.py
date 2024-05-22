85b83a0b287c1050f960f552bde25e8f3379fe9f
Ashash94 <asanches.devia@gmail.com>
2024-05-22 18:29:05 +0200

    Commit du code torch

--- graph_torch.py
+++ graph_torch.py
@@ -1,5 +1,14 @@
-import os
-import torch
+from torch_geometric.datasets import Planetoid
+from torch_geometric.transforms import NormalizeFeatures
 
-os.environ["TORCH"] = torch.__version__
-os.environ["PYTHONWARNINGS"] = "ignore"
+dataset = Planetoid(root='data/Planetoid', name = 'Cora', transform = NormalizeFeatures())
+
+print(f'Dataset: {dataset}')
+print('======================')
+print(f'Number of graphs : {len(dataset)}')
+print(f'Number of features : {dataset.num_features}')
+print(f'Number of classes : {dataset.num_classes}')
+
+data = dataset[0]
+
+print(data)
